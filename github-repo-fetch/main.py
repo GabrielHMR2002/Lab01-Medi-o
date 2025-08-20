235ddb941a56643d102678d30d7ce7ee7c11dd06
gabriel <gabriel.h.miranda.rodrigues@gmail.com>
2025-08-20 17:05:14 -0300

    Change variable to get 1000 repos

--- github-repo-fetch/main.py
+++ github-repo-fetch/main.py
@@ -4,7 +4,7 @@ from src.report import generate_report
 import pandas as pd
 
 if __name__ == "__main__":
-    repos = fetch_repositories(total_repos=20, per_page=10)
+    repos = fetch_repositories(total_repos=1000, per_page=10)
     save_csv(repos, "data/repositories.csv")
     save_json(repos, "data/repositories.json")
     generate_report(repos)
